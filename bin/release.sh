4682dd8caab345ffde3858b80f3d37054db39c63
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-03-15 19:16:12 +0000

    add date to revision

--- bin/release.sh
+++ bin/release.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 VERSION=0.0.0
-echo $VERSION-$(git rev-parse --short HEAD) > pkg/build/REVISION
+
+date=$(date '+%Y%m%d')
+echo $VERSION-$date-$(git rev-parse --short HEAD) > pkg/build/REVISION
 
 mkdir -p out
 
 go generate ./...
-go build -o out/fuddle cmd/fuddle/main.go
+go build -o out/fuddle main.go
