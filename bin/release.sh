eb853fbd94bd333a20217550284f1329feee72a5
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-03-16 18:59:24 +0000

    remove newline from REVISION

--- bin/release.sh
+++ bin/release.sh
@@ -3,7 +3,7 @@
 VERSION=0.0.0
 
 date=$(date '+%Y%m%d')
-echo $VERSION-$date-$(git rev-parse --short HEAD) > pkg/build/REVISION
+echo -n $VERSION-$date-$(git rev-parse --short HEAD) > pkg/build/REVISION
 
 mkdir -p out
 
