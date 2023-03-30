1d90da2bf7ea323a8bfb777adc1b07a9bfead667
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-03-30 19:11:13 +0100

    cli: rename status command to info

--- pkg/cli/info/command.go
+++ pkg/cli/info/command.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package status
+package info
 
 import (
 	"context"
@@ -27,7 +27,7 @@ import (
 )
 
 var Command = &cobra.Command{
-	Use:   "status",
+	Use:   "info",
 	Short: "inspect the status of the cluster",
 }
 
--- pkg/cli/info/flags.go
+++ pkg/cli/info/flags.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package status
+package info
 
 var (
 	// addr is the Fuddle registry server to query.
