8a147dc5634b67b473c2c786f67168b00acdb7bc
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-04-19 06:48:20 +0100

    registryv2: register metrics

---
+++ pkg/registryv2/registry/metrics.go
@@ -0,0 +1,32 @@
+package registry
+
+import (
+	"github.com/fuddle-io/fuddle/pkg/metrics"
+)
+
+type Metrics struct {
+	MembersCount *metrics.Gauge
+	MembersOwned *metrics.Gauge
+}
+
+func NewMetrics() *Metrics {
+	return &Metrics{
+		MembersCount: metrics.NewGauge(
+			"registry",
+			"members.count",
+			[]string{"status", "service", "owner"},
+			"Number of members in the registry",
+		),
+		MembersOwned: metrics.NewGauge(
+			"registry",
+			"members.owned",
+			[]string{"status", "service"},
+			"Number of members owned by the node",
+		),
+	}
+}
+
+func (m *Metrics) Register(collector metrics.Collector) {
+	collector.AddGauge(m.MembersCount)
+	collector.AddGauge(m.MembersOwned)
+}
