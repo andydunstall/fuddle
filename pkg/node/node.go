f4191a57a2f166bb64ed31916124a1e673fe9bc6
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-04-22 09:49:40 +0100

    node: remove registryv2

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -7,15 +7,12 @@ import (
 	rpc "github.com/fuddle-io/fuddle-rpc/go"
 	adminServer "github.com/fuddle-io/fuddle/pkg/admin/server"
 	"github.com/fuddle-io/fuddle/pkg/cluster"
-	clusterv2 "github.com/fuddle-io/fuddle/pkg/clusterv2"
 	"github.com/fuddle-io/fuddle/pkg/config"
 	"github.com/fuddle-io/fuddle/pkg/gossip"
 	"github.com/fuddle-io/fuddle/pkg/logger"
 	"github.com/fuddle-io/fuddle/pkg/metrics"
 	"github.com/fuddle-io/fuddle/pkg/registry"
 	registryServer "github.com/fuddle-io/fuddle/pkg/registry/server"
-	registryv2 "github.com/fuddle-io/fuddle/pkg/registryv2/registry"
-	registryServerv2 "github.com/fuddle-io/fuddle/pkg/registryv2/server"
 	rpcServer "github.com/fuddle-io/fuddle/pkg/server"
 	"go.uber.org/zap"
 )
@@ -26,8 +23,6 @@ type Node struct {
 
 	gossip      *gossip.Gossip
 	registry    *registry.Registry
-	registryv2  *registryv2.Registry
-	clusterv2   *clusterv2.Cluster
 	rpcServer   *rpcServer.Server
 	adminServer *adminServer.Server
 
@@ -71,14 +66,11 @@ func NewNode(conf *config.Config, opts ...Option) (*Node, error) {
 		registry.WithLogger(logger.Logger("registry")),
 	)
 
-	regv2 := registryv2.NewRegistry(conf.NodeID, time.Now().UnixMilli())
-
 	c := cluster.NewCluster(
 		r,
 		cluster.WithLogger(logger.Logger("cluster")),
 		cluster.WithCollector(collector),
 	)
-	clusterv2 := clusterv2.NewCluster(regv2)
 
 	var adminServerOpts []adminServer.Option
 	if options.adminListener != nil {
@@ -105,12 +97,11 @@ func NewNode(conf *config.Config, opts ...Option) (*Node, error) {
 	gossipOpts = append(gossipOpts, gossip.WithOnJoin(func(node gossip.Node) {
 		if node.ID != conf.NodeID {
 			c.OnJoin(node.ID, node.RPCAddr)
-			clusterv2.OnJoin(node.ID, node.RPCAddr)
 		}
 	}))
 	gossipOpts = append(gossipOpts, gossip.WithOnLeave(func(node gossip.Node) {
 		if node.ID != conf.NodeID {
-			clusterv2.OnLeave(node.ID)
+			c.OnLeave(node.ID)
 		}
 	}))
 	gossipOpts = append(gossipOpts, gossip.WithLogger(logger.Logger("gossip")))
@@ -146,13 +137,6 @@ func NewNode(conf *config.Config, opts ...Option) (*Node, error) {
 	rpc.RegisterClientWriteRegistryServer(s.GRPCServer(), clientWriteRegistryServer)
 	rpc.RegisterReplicaReadRegistryServer(s.GRPCServer(), replicaReadRegistryServer)
 
-	clientReadRegistryServerv2 := registryServerv2.NewClientReadServer()
-	clientWriteRegistryServerv2 := registryServerv2.NewClientWriteServer()
-	replicaRegistryServer := registryServerv2.NewReplicaServer()
-	rpc.RegisterClientReadRegistry2Server(s.GRPCServer(), clientReadRegistryServerv2)
-	rpc.RegisterClientWriteRegistry2Server(s.GRPCServer(), clientWriteRegistryServerv2)
-	rpc.RegisterReplicaRegistry2Server(s.GRPCServer(), replicaRegistryServer)
-
 	if err := s.Serve(); err != nil {
 		return nil, fmt.Errorf("fuddle: %w", err)
 	}
@@ -160,8 +144,6 @@ func NewNode(conf *config.Config, opts ...Option) (*Node, error) {
 	n := &Node{
 		Config:      conf,
 		registry:    r,
-		registryv2:  regv2,
-		clusterv2:   clusterv2,
 		gossip:      g,
 		rpcServer:   s,
 		adminServer: adminServer,
@@ -170,7 +152,6 @@ func NewNode(conf *config.Config, opts ...Option) (*Node, error) {
 	}
 
 	go n.failureDetector()
-	go n.replicaRepair()
 
 	return n, nil
 }
@@ -203,20 +184,6 @@ func (n *Node) failureDetector() {
 			return
 		case <-ticker.C:
 			n.registry.CheckMembersLiveness()
-		}
-	}
-}
-
-func (n *Node) replicaRepair() {
-	ticker := time.NewTicker(time.Millisecond * 500)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-n.done:
-			return
-		case <-ticker.C:
-			n.clusterv2.ReplicaRepair()
 		}
 	}
 }
