5168236ce0cd7333d4995221dae516cdfd71005e
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-04-22 07:18:33 +0100

    registryv2: add digest/delta

--- pkg/registryv2/registry/registry.go
+++ pkg/registryv2/registry/registry.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"math/rand"
 	"sync"
 
 	rpc "github.com/fuddle-io/fuddle-rpc/go"
@@ -11,6 +12,10 @@ type Registry struct {
 	localID string
 
 	members map[string]*rpc.Member2
+
+	// priorityMembers contains the member IDs that are known to be out of date
+	// so must be included as a priority in the next digest.
+	priorityMembers map[string]interface{}
 
 	// lastVersion is the last version used by this node. This is used to
 	// increment the version counter when there are multiple versions in the
@@ -39,6 +44,7 @@ func NewRegistry(localID string, timestamp int64, opts ...Option) *Registry {
 	r := &Registry{
 		localID:          localID,
 		members:          make(map[string]*rpc.Member2),
+		priorityMembers:  make(map[string]interface{}),
 		tombstoneTimeout: options.tombstoneTimeout,
 		metrics:          metrics,
 	}
@@ -197,24 +203,77 @@ func (r *Registry) RemoteUpsertMember(member *rpc.Member2) {
 }
 
 func (r *Registry) MembersDigest(maxMembers int) map[string]*rpc.MonotonicTimestamp {
-	// select maxMembers random members and return a mapping from
-	// id => version.Timestamp
-	// TODO(AD) does this need to contain the owner too? probably not given if
-	// theres a confict it will be resolved by heartbeats later
-	return nil
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	digest := make(map[string]*rpc.MonotonicTimestamp)
+
+	for id := range r.priorityMembers {
+		// The member may not exist, so if not use a timestamp of 0.
+		if m, ok := r.members[id]; ok {
+			digest[id] = &rpc.MonotonicTimestamp{
+				Timestamp: m.Version.Timestamp.Timestamp,
+				Counter:   m.Version.Timestamp.Counter,
+			}
+		} else {
+			digest[id] = &rpc.MonotonicTimestamp{
+				Timestamp: 0,
+			}
+		}
+		delete(r.priorityMembers, id)
+
+		if len(digest) >= maxMembers {
+			return digest
+		}
+	}
+
+	var memberIDs []string
+	for id := range r.members {
+		memberIDs = append(memberIDs, id)
+	}
+	memberIDs = shuffleStrings(memberIDs)
+
+	for _, id := range memberIDs {
+		digest[id] = &rpc.MonotonicTimestamp{
+			Timestamp: r.members[id].Version.Timestamp.Timestamp,
+			Counter:   r.members[id].Version.Timestamp.Counter,
+		}
+		if len(digest) >= maxMembers {
+			return digest
+		}
+	}
+
+	return digest
 }
 
 func (r *Registry) MembersDelta(digest map[string]*rpc.MonotonicTimestamp) []*rpc.Member2 {
-	// check the digest for any members we don't know about or we are out of
-	// date on
-	// return our own digest containing a digest of these members we need so
-	// we can use push/pull
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-	// check the digest for any members we are more up to date on and return
-	// (note as digest only partital we don't know what members the sender is
-	// missing)
+	// Compare our local state with the requested version in the digest.
+	// If our version is greater, then we include the full member in the
+	// response. Otherwise if either we don't know about the member, or the
+	// sender is more up to date, then we save the member ID as a 'priority
+	// member' to be included in the next digest as a priority.
+	//
+	// Note only compare timestamps, not owner IDs. If there is a conflict where
+	// multiple nodes took ownership of a member in the same millisecond, it
+	// will be resolved later so it doesn't matter which we pick.
 
-	return nil
+	var members []*rpc.Member2
+	for id, timestamp := range digest {
+		if m, ok := r.members[id]; ok {
+			if compareTimestamps(timestamp, m.Version.Timestamp) > 0 {
+				members = append(members, copyMember(m))
+			} else {
+				r.priorityMembers[id] = struct{}{}
+			}
+		} else {
+			r.priorityMembers[id] = struct{}{}
+		}
+	}
+
+	return members
 }
 
 func (r *Registry) OnNodeJoin(id string) {
@@ -343,4 +402,29 @@ func compareVersions(lhs *rpc.Version2, rhs *rpc.Version2) int {
 		return -1
 	}
 	return 0
+}
+
+func compareTimestamps(lhs *rpc.MonotonicTimestamp, rhs *rpc.MonotonicTimestamp) int {
+	if lhs.Timestamp < rhs.Timestamp {
+		return 1
+	}
+	if lhs.Timestamp > rhs.Timestamp {
+		return -1
+	}
+	if lhs.Counter < rhs.Counter {
+		return 1
+	}
+	if lhs.Counter > rhs.Counter {
+		return -1
+	}
+	return 0
+}
+
+func shuffleStrings(src []string) []string {
+	res := make([]string, len(src))
+	perm := rand.Perm(len(src))
+	for i, v := range perm {
+		res[v] = src[i]
+	}
+	return res
 }
