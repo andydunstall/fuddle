9601f08c541cf7ec4df70ed3be38a71bf84cda2a
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-04-23 05:35:47 +0100

    registry: rename servers

--- pkg/registry/server/clientreadserver.go
+++ pkg/registry/server/clientreadserver.go
@@ -9,9 +9,9 @@ import (
 	"go.uber.org/zap"
 )
 
-// ClientReadRegistryServer serves updates to the registry to the external
+// ClientReadServer serves updates to the registry to the external
 // clients.
-type ClientReadRegistryServer struct {
+type ClientReadServer struct {
 	registry *registry.Registry
 
 	outboundUpdates *metrics.Counter
@@ -20,7 +20,7 @@ type ClientReadRegistryServer struct {
 	rpc.UnimplementedClientReadRegistryServer
 }
 
-func NewClientReadRegistryServer(reg *registry.Registry, opts ...Option) *ClientReadRegistryServer {
+func NewClientReadServer(reg *registry.Registry, opts ...Option) *ClientReadServer {
 	options := defaultOptions()
 	for _, o := range opts {
 		o.apply(options)
@@ -36,7 +36,7 @@ func NewClientReadRegistryServer(reg *registry.Registry, opts ...Option) *Client
 		options.collector.AddCounter(outboundUpdates)
 	}
 
-	return &ClientReadRegistryServer{
+	return &ClientReadServer{
 		registry:        reg,
 		outboundUpdates: outboundUpdates,
 		logger:          options.logger,
@@ -45,8 +45,8 @@ func NewClientReadRegistryServer(reg *registry.Registry, opts ...Option) *Client
 
 // Updates streams updates to the local registry. This includes sending any
 // updates the client missed given their known members in the subscribe request.
-func (s *ClientReadRegistryServer) Updates(req *rpc.SubscribeRequest, stream rpc.ClientReadRegistry_UpdatesServer) error {
-	logger := s.logger.With(zap.String("rpc", "ClientReadRegistryServer.Updates"))
+func (s *ClientReadServer) Updates(req *rpc.SubscribeRequest, stream rpc.ClientReadRegistry_UpdatesServer) error {
+	logger := s.logger.With(zap.String("rpc", "ClientReadServer.Updates"))
 	logger.Debug("updates stream")
 
 	unsubscribe := s.registry.Subscribe(req, func(update *rpc.Member2) {
@@ -71,8 +71,8 @@ func (s *ClientReadRegistryServer) Updates(req *rpc.SubscribeRequest, stream rpc
 }
 
 // Member looks up the requested member.
-func (s *ClientReadRegistryServer) Member(ctx context.Context, req *rpc.MemberRequest) (*rpc.MemberResponse, error) {
-	logger := s.logger.With(zap.String("rpc", "ClientReadRegistryServer.Member"))
+func (s *ClientReadServer) Member(ctx context.Context, req *rpc.MemberRequest) (*rpc.MemberResponse, error) {
+	logger := s.logger.With(zap.String("rpc", "ClientReadServer.Member"))
 
 	m, ok := s.registry.Member(req.Id)
 	if !ok {
@@ -88,8 +88,8 @@ func (s *ClientReadRegistryServer) Member(ctx context.Context, req *rpc.MemberRe
 }
 
 // Members lists the members in the registry.
-func (s *ClientReadRegistryServer) Members(context.Context, *rpc.MembersRequest) (*rpc.MembersResponse, error) {
-	logger := s.logger.With(zap.String("rpc", "ClientReadRegistryServer.Members"))
+func (s *ClientReadServer) Members(context.Context, *rpc.MembersRequest) (*rpc.MembersResponse, error) {
+	logger := s.logger.With(zap.String("rpc", "ClientReadServer.Members"))
 
 	members := s.registry.Members()
 	logger.Debug("members request", zap.Int("num-members", len(members)))
